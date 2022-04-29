fc4ec6f1b4285e776a89f5ff0bf930fec3fea919
kenfang <kenfangch@yahoo.com.tw>
2022-04-29 15:34:45 +0800

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,11 @@
+extern crate zero2prod;
+
+use std::net::TcpListener;
+use zero2prod::run;
+
+#[tokio::main]
+async fn main() -> std::io::Result<()> {
+    let address = TcpListener::bind("127.0.0.1:0")?;
+
+    run(address)?.await
+}
