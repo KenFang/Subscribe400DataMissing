693bd6b2663dbef96b55a472fea5783641f46bcb
KenFang <kenfangch@yahoo.com.tw>
2022-05-04 18:32:22 +0800

    Add files via upload

--- src/lib.rs
+++ src/lib.rs
@@ -6,11 +6,24 @@ async fn health_check() -> HttpResponse {
     HttpResponse::Ok().finish()
 }
 
+#[derive(serde::Deserialize)]
+struct FormData {
+    email: String,
+    name: String,
+}
+
+// Let's start simple: We always return a 200 Ok
+async fn subscribe(_form: web::Form<FormData>) -> HttpResponse {
+    HttpResponse::Ok().finish()
+}
+
 // Return 'Server' on the happy path
 pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
     let server = HttpServer::new(||  {
         App::new()
             .route("/health_check", web::get().to(health_check))
+            // A new entry in our routing table for POST /subscriptions requests
+            .route("/subscriptions", web::post().to(subscribe))
     })
         .listen(listener)?
         .run();
